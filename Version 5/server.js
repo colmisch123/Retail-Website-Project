31ea0d46238c2932a5770d75e9de264372bf0104
Cole <colemische@gmail.com>
2025-11-13 11:57:49 -0600

    Attempting to get a basic 404 page running

--- Version 5/server.js
+++ Version 5/server.js
@@ -5,8 +5,12 @@ app.set("views", "templates")
 app.set("view engine", "pug")
 
 
-//app.use("/html", express.static("some_dumb_files"))
-app.use("/api", express.json())
+app.use("/html", express.static("/static"))
+
+app.get("/404", (req, res) => {
+    res.status(404)
+    res.render('404.pug')
+})
 
 
 app.listen(4131, () => {
